5e01b4a667c365d8760f6debe968c56a54ca8f50
aleskruba <74367962+aleskruba@users.noreply.github.com>
2022-12-11 11:04:56 +0100

    Update authContext.js

--- src/context/authContext.js
+++ src/context/authContext.js
@@ -14,14 +14,14 @@ export const AuthContextProvider = ({children}) =>{
 
 
  const login = async(inputs)=> {
-     const res = await axios.post("/auth/login",inputs)
+     const res = await axios.post("https://studend-app-backend-production.up.railway.app/auth/login",inputs)
     setCurrentUser(res.data)
   
 
 }
 
     const logout = async(inputs) =>{
-         await axios.post("/auth/logout") 
+         await axios.post("https://studend-app-backend-production.up.railway.app/auth/logout") 
         setCurrentUser(null)
       }
 
